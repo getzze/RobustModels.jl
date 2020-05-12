99a1ca4e8bbddcc5ec3ce03db57fe1e63e2f5743
getzze <getzze@gmail.com>
2020-05-12 15:24:14 +0100

    compatibility with julia 1.0

--- src/RobustModels.jl
+++ src/RobustModels.jl
@@ -1,17 +1,6 @@
 module RobustModels
 
-
-# https://github.com/JuliaLang/julia/pull/29679
-@static if VERSION < v"1.1.0-DEV.472"
-    isnothing(::Any) = false
-    isnothing(::Nothing) = true
-end
-
-# https://github.com/JuliaLang/julia/pull/32148
-@static if VERSION < v"1.3.0"
-    import Base: print
-    print(io::IO, ::Nothing) = print(io, "nothing")
-end
+include("compat.jl")
 
 using Distributions: ccdf, pdf, quantile, Normal, Chisq
 using SparseArrays: SparseMatrixCSC
---
+++ src/compat.jl
@@ -0,0 +1,18 @@
+## Compatibility layer to work with Julia 1.0
+
+# https://github.com/JuliaLang/julia/pull/29679
+@static if VERSION < v"1.1.0-DEV.472"
+    isnothing(::Any) = false
+    isnothing(::Nothing) = true
+end
+
+# https://github.com/JuliaLang/julia/pull/32148
+@static if VERSION < v"1.3.0"
+    import Base: print
+    print(io::IO, ::Nothing) = print(io, "nothing")
+end
+
+# https://github.com/JuliaLang/julia/pull/29173
+@static if VERSION < v"1.1.0"
+    import Future: copy!
+end
