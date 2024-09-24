0e3a958b659ff9bbc75d0fc89d832471cfe46f9d
getzze <getzze@gmail.com>
2024-09-24 14:32:26 +0100

    use format-check from MixedModels.jl

---
+++ format/run.jl
@@ -0,0 +1,26 @@
+using JuliaFormatter
+
+function main()
+    perfect = true
+    # note: keep in sync with `.JuliaFormatter.toml`
+    # and with `.github/workflows/CI.yml` format-check
+    for d in ["src/", "test/", "docs/"]
+        @info "...linting $d ..."
+        dir_perfect = format(
+            d;
+            style=BlueStyle(),
+            join_lines_based_on_source=true,
+            remove_extra_newlines = false,
+        )
+        perfect = perfect && dir_perfect
+    end
+    if perfect
+        @info "Linting complete - no files altered"
+    else
+        @info "Linting complete - files altered"
+        run(`git status`)
+    end
+    return nothing
+end
+
+main()
